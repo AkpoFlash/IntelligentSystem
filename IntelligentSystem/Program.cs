d0ef0182a8d458045669c68d40826754b7801f9c
Анисимов Кирилл <k.anisimov@maximaster.ru>
2017-07-19 17:01:31 +0300

    Add discount system for store and refactor some code

---
+++ IntelligentSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelligentSystem
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IntelligentSystem.InitializeProduct();
+            IntelligentSystem.InitializeWorkedPeople();
+
+            Console.WriteLine("Products before discount for category:");
+            IntelligentSystem.OutputProducts();
+
+            IntelligentSystem.DiscountForCategories();
+
+            Console.WriteLine("Products after discount for category:");
+            IntelligentSystem.OutputProducts();
+
+            IntelligentSystem.CalculateRelevantProducts();
+
+            Console.WriteLine("Result products for customers:");
+            IntelligentSystem.OutputResult();
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
